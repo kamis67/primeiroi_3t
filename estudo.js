c9859a2801ebc04b6e9cefb1be5b4b74d70c6b62
kamis67 <110511424+kamis67@users.noreply.github.com>
2022-11-12 11:32:22 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,55 +1,49 @@
-/// alert("Oi! Eu sou o Alert");
-// document.write("Estou na página.");
-// console.log("Eu estou no console.");
-let tabuada = 5;
-function alo(){
+alerta ("Oi! Eu sou o Alerta");
+document.write("Estou na página.");
+console.log("Eu estou no console.");
+deixetabuada=5;
+funçãoalo(){
+    deixetabuada=documento. getElementById("entrada"). valor;
 
-    let tabuada = document.getElementById("entrada").value;
+    documento. escrever ("<h1>Tabuada do "+tabuada+"</h1>");
+    documento. escrever (tabuada+" x 1 = "+(tabuada*1)+"<br>");
+    documento. escrever (tabuada+" x 2 = "+(tabuada*2)+"<br>");
+    documento. escrever (tabuada+" x 3 = "+(tabuada*3)+"<br>");
+    documento. escrever (tabuada+" x 4 = "+(tabuada*4)+"<br>");
+    documento. escrever (tabuada+" x 5 = "+(tabuada*5)+"<br>");
+    documento. escrever (tabuada+" x 6 = "+(tabuada*6)+"<br>");
+    documento. escrever (tabuada+" x 7 = "+(tabuada *7)+"<br>");
+    documento. escrever (tabuada+" x 8 = "+(tabuada *8)+"<br>");
+    documento. escrever (tabuada+" x 9 = "+(tabuada*9)+"<br>");
+    documento. escrever (tabuada+" x 10 = "+(tabuada*10));
+       
+}
+funçãoescreva(){
+  para(vari=1; i<=10; eu++){
+     para(varj=1; j<=10; j++){
+documento. write(i+" x "+j+" = "+(j*i)+"<br>");
+     }
+    documento. escrever ("<br>");
+ }
 
-    document.write("<h1>Tabuada do " + tabuada + "</h1>");
-    document.write(tabuada + " x 1 = " + (tabuada*1)+"<br>");
-    document.write(tabuada + " x 2 = " + (tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 = " + (tabuada*3)+"<br>");
-    document.write(tabuada + " x 4 = " + (tabuada*4)+"<br>");
-    document.write(tabuada + " x 5 = " + (tabuada*5)+"<br>");
-    document.write(tabuada + " x 6 = " + (tabuada*6)+"<br>");
-    document.write(tabuada + " x 7 = " + (tabuada*7)+"<br>");
-    document.write(tabuada + " x 8 = " + (tabuada*8)+"<br>");
-    document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10));
 }
-// for(inicio, validaçao, manutençao)
-function escreva(){
-    for(var i=1; i <= 10; i++){
-        for(var j = 1; j <=10; j++){
-            document.write(i + " x " + j + " = " + (j*i)+"<br>");
-        }
-        document.write("<br>");
-    }
+funçãomoeda (atual){
+    retornoatual. toLocaleString ('pt-br',{style:'currency',currency:'BRL'});
 }
-
-function quadrado(){
-    for(var i = 2; i < 11; i++){
-        document.write("O quadrado de " + i + " é " + (i*i)+"<br>");
-    }
-}
-function moeda(atual){
-    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-}
-function total(){
-    let val = document.getElementById("valor").value;
-    let ju = document.getElementById("juros").value;
+total função(){
+    deixeval=documento. getElementById("valor"). valor;
+    deixeju=documento. getElementById("juros"). valor;
     
-    let t = document.getElementById("meses").value;
-    let resultado = 0;
-    let saida = "";
-    for(let m = 1; m <= t; m++){
+    deixet=documento. getElementById("meses"). valor;
+    deixarresultado=0;
+    deixesaida = "";
+    para(deixem=1; m<=t; m++){
         resultado = (val * ((ju/100)+1));
         val = resultado;
-        saida += "Mês " + m + ": " + moeda(val) + "<br>";
-        //document.write("Mês " + m + " valor de " + moeda(val) + "<br>");
+        saida += "Mês" + m + ":" + moeda(val) + "<br>";
+        document.write("Mês " + m + " valor de " + moeda(val) + "<br>");
     }
-    document.getElementById("mes").innerHTML = saida;
-    document.getElementById("resultado").innerHTML = "Total: " + moeda(resultado);
-   //document.write("O total é de: " + moeda(resultado) );
+    documento. getElementById("mes"). innerHTML=saida
+    documento. getElementById("resultado"). innerHTML="Total: "+moeda(resultado);
+   document.write("O total é de: " + moeda(resultado));
 }
